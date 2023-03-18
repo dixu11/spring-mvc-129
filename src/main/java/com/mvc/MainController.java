9e1839f7b72b04c387fdae6d5cd374383e11fe4e
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-03-18 12:14:38 +0100

    css + js included

--- src/main/java/com/mvc/MainController.java
+++ src/main/java/com/mvc/MainController.java
@@ -1,16 +1,17 @@
 package com.mvc;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
     @RequestMapping(path = "/imperator",method = RequestMethod.GET)
-    public void getImperatorPage() {
-        System.out.println("Odpalamy stronkę z zarządzeniem imperium");
+    public ModelAndView getImperatorPage() {
+        ModelAndView modelAndView = new ModelAndView("imperator-page.html");
+        return modelAndView;
     }
 }
