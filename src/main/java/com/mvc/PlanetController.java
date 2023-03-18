7e1dbc1fc18976355c16133bd12b7b5836cf5e5f
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-03-18 12:23:49 +0100

    hiperlink to planet creation section

---
+++ src/main/java/com/mvc/PlanetController.java
@@ -0,0 +1,15 @@
+package com.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PlanetController {
+
+    @RequestMapping(path = "/add-planet", method = RequestMethod.GET)
+    public ModelAndView getPlanetForm() {
+        return new ModelAndView("planet-form.html");
+    }
+}
