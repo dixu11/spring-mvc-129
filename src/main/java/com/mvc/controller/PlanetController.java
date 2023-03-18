ba4bc252bba480e65bf998ec6ff060b25a0219c5
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-03-18 14:32:00 +0100

    create PlanetCreationRequest transfer object and delete creation to PlanetService

--- src/main/java/com/mvc/controller/PlanetController.java
+++ src/main/java/com/mvc/controller/PlanetController.java
@@ -1,5 +1,7 @@
 package com.mvc.controller;
 
+import com.mvc.request.PlanetCreationRequest;
+import com.mvc.service.PlanetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PlanetController {
 
+    private PlanetService planetService;
+
+    public PlanetController(PlanetService planetService) {
+        this.planetService = planetService;
+    }
+
     @RequestMapping(path = "/add-planet", method = RequestMethod.GET)
     public ModelAndView getPlanetForm() {
         return new ModelAndView("planet-form.html");
@@ -19,9 +27,7 @@ public class PlanetController {
     public String createPlanet(@RequestParam String name,
                                @RequestParam(name = "planet_type") String planetType,
                                @RequestParam int size) {
-        System.out.println(name);
-        System.out.println(planetType);
-        System.out.println(size);
+        planetService.createPlanet(new PlanetCreationRequest(name,planetType,size));
         return "imperator-page";
     }
 }
