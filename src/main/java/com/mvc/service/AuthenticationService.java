da8a8e424f46994817f394a03f0081730f4e8f8a
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-03-19 09:40:47 +0100

    add form to create new account (Imperator) with unique name. Send data as RegisterRequest to AuthenticationService

---
+++ src/main/java/com/mvc/service/AuthenticationService.java
@@ -0,0 +1,13 @@
+package com.mvc.service;
+
+import com.mvc.request.RegisterRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public void createImperator(RegisterRequest request) {
+
+    }
+
+}
